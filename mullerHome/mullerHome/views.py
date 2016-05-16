279179abadb6bdcbd6484813a033a7ca7b510bc4
RoknarHub <ian.muller@mykolab.com>
2016-05-16 17:58:15 +0200

    minor fix

--- mullerHome/mullerHome/views.py
+++ mullerHome/mullerHome/views.py
@@ -19,3 +19,16 @@ def curriculum(request):
         response['Content-Disposition'] = 'inline;filename=MULLER_Ian_Curriculum_Vitae.pdf'
         return response
     pdf.closed
+
+
+import locale
+import sys
+
+
+def view_locale(request):
+    loc_info = "getlocale: " + str(locale.getlocale()) + \
+               "<br/>getdefaultlocale(): " + str(locale.getdefaultlocale()) + \
+               "<br/>fs_encoding: " + str(sys.getfilesystemencoding()) + \
+               "<br/>sys default encoding: " + str(sys.getdefaultencoding())
+    "<br/>sys default encoding: " + str(sys.getdefaultencoding())
+    return HttpResponse(loc_info)
