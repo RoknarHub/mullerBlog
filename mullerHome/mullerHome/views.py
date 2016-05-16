77d2955e57f8997d2b767db0b383e38ff94fbc8b
RoknarHub <ian.muller@mykolab.com>
2016-05-16 17:06:42 +0200

    minor fix

--- mullerHome/mullerHome/views.py
+++ mullerHome/mullerHome/views.py
@@ -11,7 +11,7 @@ def blogIndex(request):
 
 
 def curriculum(request):
-    with open('/home/muller/Documents/MULLER_Ian_Curriculum_Vitae.pdf', 'r') as pdf:
+    with open('/home/muller/DjangoApps/mullerHome/mullerHome/static/mullerHome/various/MULLER_Ian_Curriculum_Vitae.pdf', 'r') as pdf:
         response = HttpResponse(pdf.read(), mimetype='application/pdf')
         response['Content-Disposition'] = 'inline;filename=MULLER_Ian_Curriculum_Vitae.pdf'
         return response
