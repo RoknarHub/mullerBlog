bd9d2b1839d57b09c2375a3a0127f394e2b2fe4a
RoknarHub <ian.muller@mykolab.com>
2016-05-20 03:40:52 +0200

    updating models

--- mullerHome/mullerHome/models.py
+++ mullerHome/mullerHome/models.py
@@ -12,13 +12,13 @@ class BlogEntry(models.Model):
     blog_description = models.CharField(max_length=200)
     blog_thumbnail = models.URLField(max_length=200)
 
-    def __str__(self):
-        return self.blog_title
+    #def __str__(self):
+    #    return self.blog_title
 
-    def was_published_recently(self):
-        now = timezone.now()
-        return now-datetime.timedelta(days=1) <= self.pub_date <= now
+    #def was_published_recently(self):
+    #    now = timezone.now()
+    #    return now-datetime.timedelta(days=1) <= self.pub_date <= now
 
-    was_published_recently.admin.order_field = 'pub_date'
-    was_published_recently.boolean = True
-    was_published_recently.short_description = 'Published recently?'
+    #was_published_recently.admin.order_field = 'pub_date'
+    #was_published_recently.boolean = True
+    #was_published_recently.short_description = 'Published recently?'
